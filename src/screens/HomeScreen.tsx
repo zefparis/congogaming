4312baceffec9b69a3f859166d7a625316a54ee4
zefparis <ben.barere@gmail.com>
2026-05-23 18:34:43 +0200

    style(home): adjust trophy bottom position to 8%

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -87,7 +87,7 @@ export default function HomeScreen() {
             style={{
               position: 'absolute',
               right: '-5px',
-              bottom: '0',
+              bottom: '8%',
               height: '72%',
               width: 'auto',
               objectFit: 'contain',
